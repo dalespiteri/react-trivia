a61cfa25f964fad1b42f8fff183d258be799edbd
dalespiteri <dalespiteri@gmail.com>
2017-12-20 16:43:57 -0500

    JSON data request test

--- script/sports.js
+++ script/sports.js
@@ -1,1 +1,7 @@
-console.log('hello');
+$(document).ready(function () {
+
+  $.getJSON('https://opentdb.com/api.php?amount=10&category=21', function(triviaResults) {
+    console.log(triviaResults.results);
+  });
+
+});
